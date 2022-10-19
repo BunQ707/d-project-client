e1be8baa57f1c4adaba61d1ee7a78fd77eaae033
BunQ707 <adung2912d03@gmail.com>
2022-10-19 15:15:48 +0700

    change be url

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,4 +1,4 @@
-const API_URL_BACKEND = process.env.NEXT_PUBLIC_API_URL_BACKEND || 'http://localhost:3000';
+const API_URL_BACKEND = process.env.NEXT_PUBLIC_API_URL_BACKEND || 'http://localhost:3001';
 
 export const envConfig = {
   API_URL_BACKEND,
