f555a9db5dfe6309418691029ce4fa708971aec5
BunQ707 <adung2912d03@gmail.com>
2022-10-21 16:38:44 +0700

    fix api

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,21 +1,14 @@
-import axios from 'axios';
+import { LoginDto, RegisterDto } from 'utils/auth.dto';
 import { apiClient } from '../utils/axios';
 
 export interface LoginResponse {
-  data: { accessToken: string; refreshToken: string };
+  data: { accessToken: string };
 }
 
-export const login = (params: Record<string, unknown>): Promise<LoginResponse> => {
-  return apiClient.post('api/auth/sign-in', params);
+export const login = (params: LoginDto): Promise<LoginResponse> => {
+  return apiClient.post('login', params);
 };
 
-export const loginA8 = (tokenA8: string, newWalletAddress?: string) => {
-  return apiClient.request({
-    method: 'POST',
-    url: 'api/auth/sign-in-a8',
-    data: {
-      token: tokenA8,
-      ...(newWalletAddress && { walletAddressInput: newWalletAddress }),
-    },
-  });
+export const register = (params: RegisterDto) => {
+  return apiClient.post('signin', params);
 };
