18d7ddd45606470fc0137c89b03409d7ef3d0078
BunQ707 <adung2912d03@gmail.com>
2022-10-25 16:53:32 +0700

    fix avatar

--- src/components/shared/AccountBadge.tsx
+++ src/components/shared/AccountBadge.tsx
@@ -23,7 +23,7 @@ const AccontBadge = () => {
     <Box sx={{ flexGrow: 0 }}>
       <Tooltip title="Open settings">
         <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-          <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+          <Avatar alt="Remy Sharp" src="/avatar.png" />
         </IconButton>
       </Tooltip>
       <Menu
