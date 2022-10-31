fbd9c0edf030c3440f2014ed11cda312d6e4ab6e
BunQ707 <adung2912d03@gmail.com>
2022-10-31 09:27:48 +0700

    refactor footer

--- src/components/shared/Footer.tsx
+++ src/components/shared/Footer.tsx
@@ -1,29 +1,5 @@
 const Footer: React.FC = () => {
-  return (
-    <footer className="">
-      {/* <div className="h-[1px] max-w-[1196px] w-full mb-[70px]"></div>
-      <p className="mb-[38px] text-center text-white text-[20px]">Join Our Community</p>
-      <div className="flex flex-wrap justify-center gap-[22px] mb-[60px]">
-        {listIconFooters.map((item) => {
-          return (
-            <a href={item.href} target="_blank" key={item.href}>
-              <div
-                key={item.src}
-                className="cursor-pointer flex justify-center items-center w-[40px] h-[40px] opacity-70"
-              >
-                <img src={URL_BASE_PATH + item.src} alt="icon" />
-              </div>
-            </a>
-          );
-        })}
-      </div>
-      <div className="flex items-center mb-[15px] mt-[14px]">
-        <img className="w-[39px] h-[39px] mr-[12px]" src={URL_BASE_PATH + "/img/logo.png"} />
-        <div className="text-[#fafafab3] text-[28px]">ANCIENT8</div>
-      </div>
-      <p className="mb-[26px] text-center text-[#fafafab3]">Copyright © 2022 Ancient8. All Rights Reserved. </p> */}
-    </footer>
-  );
+  return <footer className=""></footer>;
 };
 
 export default Footer;
